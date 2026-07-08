f3b9592336c2559b88bbc664f984efa56f0756cb
Nanako-Murata <mokalate.nanako@gmail.com>
2026-07-08 16:25:43 +0900

    commit

--- backend/src/main/java/com/example/demo/config/WebFluxCorsConfig.java
+++ backend/src/main/java/com/example/demo/config/WebFluxCorsConfig.java
@@ -9,18 +9,18 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 @Configuration
 public class WebFluxCorsConfig {
 
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("https://frontend.victorioussmoke-9874bef0.japaneast.azurecontainerapps.io");
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");
-        config.setAllowCredentials(false);
+	 @Bean
+	    public CorsWebFilter corsWebFilter() {
+	        CorsConfiguration config = new CorsConfiguration();
+	        config.addAllowedOrigin("https://frontend.victorioussmoke-9874bef0.japaneast.azurecontainerapps.io");
+	        config.addAllowedMethod("*");
+	        config.addAllowedHeader("*");
+	        config.setAllowCredentials(false);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        source.registerCorsConfiguration("/**", config);
 
-        return new CorsWebFilter(source);
+	        return new CorsWebFilter(source);
     }
 
 }
